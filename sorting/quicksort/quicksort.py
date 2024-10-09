e4ef57fe723d764c8388d2fbf6e74bee0920a203
Nico <negretenico@gmail.com>
2024-10-09 08:46:47 -0400

    adding the partition

--- sorting/quicksort/quicksort.py
+++ sorting/quicksort/quicksort.py
@@ -1,5 +1,14 @@
 from typing import List
 
+def partition(arr:List[int],lo:int,hi:int) -> int:
+    pivot = arr[hi] 
+    temp_pivot = lo-1
+    for j in range(lo,hi-1):
+        if arr[j] < pivot:
+            temp_pivot +=1
+            arr[temp_pivot], arr[j] = arr[j],arr[temp_pivot]
+    arr[temp_pivot+1], arr[hi] = arr[hi], arr[temp_pivot+1]
+    return temp_pivot+1
 
 def quicksort(arr:List[int]) -> List[int]:
     return []
